4b85c543da1ffcb3257462b2be27982e13415098
Oscar Duran <xoscarduranx@gmail.com>
2017-05-07 00:02:43 +0200

    Initial version

--- src/states/Game.js
+++ src/states/Game.js
@@ -4,31 +4,92 @@ import Mushroom from '../sprites/Mushroom'
 
 export default class extends Phaser.State {
   init () {}
-  preload () {}
+  preload () {
+    this.game.load.spritesheet('player','./assets/images/player.png',28,22)
+    this.game.load.spritesheet('ground','./assets/images/ground.png')
+    this.game.load.spritesheet('wall','./assets/images/wall.png')
+    this.game.load.spritesheet('enemy','./assets/images/enemy.png')
+    this.game.load.spritesheet('coin','./assets/images/coin.png')
+
+    this.game.load.audio('soundjump','./assets/images/jump.mp3','./assets/images/jump.wav')
+  }
 
   create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
-    banner.font = 'Bangers'
-    banner.padding.set(10, 16)
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.smoothed = false
-    banner.anchor.setTo(0.5)
+    this.game.physics.startSystem(Phaser.Physics.ARCADE)
 
-    this.mushroom = new Mushroom({
-      game: this,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
+    this.player = this.game.add.sprite(350,50,'player')
+    this.ground = this.game.add.sprite(700/2-160,400/2,'ground')
+    this.wall1  = this.game.add.sprite(700/2-160,400/2-80,'wall')
+    this.wall2  = this.game.add.sprite(700/2+140,400/2-80,'wall')
+    this.enemy  = this.game.add.sprite(700/2+120,400/2-20,'enemy')
+    this.coin1  = this.game.add.sprite(700/2-120,400/2-20,'coin')
+    this.coin2  = this.game.add.sprite(700/2-80,400/2-20,'coin')
+    this.coin3  = this.game.add.sprite(700/2-40,400/2-20,'coin')
 
-    this.game.add.existing(this.mushroom)
+    this.jump = this.game.add.audio('soundjump')
+
+    game.physics.arcade.enable(this.player)
+    game.physics.arcade.enable(this.ground)
+    game.physics.arcade.enable(this.wall1)
+    game.physics.arcade.enable(this.wall2)
+    game.physics.arcade.enable(this.enemy)
+    game.physics.arcade.enable(this.coin1)
+    game.physics.arcade.enable(this.coin2)
+    game.physics.arcade.enable(this.coin3)
+
+    this.player.body.gravity.y=600
+    this.player.body.setSize(20,20,0,0);
+
+    this.ground.body.immovable = true
+    this.wall1.body.immovable = true
+    this.wall2.body.immovable = true
+    this.enemy.body.immovable = true
+
+
+    this.player.animations.add('idle',[3,4,5,4],5,true)
+    this.player.animations.play('idle')
+    this.cursor = game.input.keyboard.createCursorKeys()
+    this.hasJumped= false
+  }
+
+  update(){
+    this.game.physics.arcade.collide(this.player, this.ground)
+    this.game.physics.arcade.collide(this.player, this.wall1)
+    this.game.physics.arcade.collide(this.player, this.wall2)
+    this.game.physics.arcade.collide(this.player, this.enemy)
+    this.game.physics.arcade.collide(this.player, this.coin1)
+    this.game.physics.arcade.collide(this.player, this.coin2)
+    this.game.physics.arcade.collide(this.player, this.coin3)
+
+    this.inputs()
+  }
+
+  inputs(){
+      if (this.cursor.left.isDown) {
+          this.player.body.velocity.x= -100
+          this.player.frame = 2
+      }else if(this.cursor.right.isDown) {
+          this.player.body.velocity.x= +100
+          this.player.frame = 2
+      }
+      else  {
+        this.player.body.velocity.x= 0
+      }
+
+      if(this.cursor.up.isDown){
+        this.jumpPlayer()
+      }
+  }
+
+  jumpPlayer(){
+      this.player.body.velocity.y= -220
+      this.hasJumped = true
+      this.jump.play()
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+
     }
   }
 }
